9e47ff342773a50e0b584fcf23b0a6df7aacb7c5
고윤혁 <sksgur3217@gmail.com>
2023-03-27 09:49:07 +0900

    :hammer: [fix] cookie 정책 대응 관련 임시 user_id admin으로 변수 설정

--- back/src/routes/todo.routes.js
+++ back/src/routes/todo.routes.js
@@ -7,10 +7,16 @@ const todoController = new TodoController();
 const { isLoggedIn } = require('../../middlewares/isLoggedIn'); // 로그인된 사용자만 접근이 가능합니다.
 
 router
-  .post('/', isLoggedIn, todoController.addTodo) // :TODO 생성
-  .get('/', isLoggedIn, todoController.getAllTodos) // :TODO 전체조회
-  .get('/:todo_id', isLoggedIn, todoController.getDetailTodo) // :TODO 상세조회
-  .patch('/:todo_id', isLoggedIn, todoController.updateTodo) // :TODO 수정
-  .delete('/:todo_id', isLoggedIn, todoController.deleteTodo); // :TODO 삭제
+  .post('/', todoController.addTodo) // :TODO 생성
+  .get('/', todoController.getAllTodos) // :TODO 전체조회
+  .get('/:todo_id', todoController.getDetailTodo) // :TODO 상세조회
+  .patch('/:todo_id', todoController.updateTodo) // :TODO 수정
+  .delete('/:todo_id', todoController.deleteTodo); // :TODO 삭제
+
+// .post('/', isLoggedIn, todoController.addTodo) // :TODO 생성
+// .get('/', isLoggedIn, todoController.getAllTodos) // :TODO 전체조회
+// .get('/:todo_id', isLoggedIn, todoController.getDetailTodo) // :TODO 상세조회
+// .patch('/:todo_id', isLoggedIn, todoController.updateTodo) // :TODO 수정
+// .delete('/:todo_id', isLoggedIn, todoController.deleteTodo); // :TODO 삭제
 
 module.exports = router;
--- back/src/routes/user.routes.js
+++ back/src/routes/user.routes.js
@@ -13,19 +13,17 @@ const userController = new UserController();
 const { isNotLoggedIn } = require('../../middlewares/isNotLoggedIn'); // 로그인되지 않은 사용자만 접근이 가능합니다.
 const { isLoggedIn, isLoggedInForLogout } = require('../../middlewares/isLoggedIn'); // 로그인된 사용자만 접근이 가능합니다.
 
-// 회원가입
-router.post('/signup', isNotLoggedIn, signupController.signup);
+router
+  .post('/signup', signupController.signup) // 회원가입
+  .post('/login', loginController.login) // 로그인
+  .post('/logout', loginController.logout) // 로그아웃
+  .get('/', userController.findUser) // 내 정보 조회
+  .patch('/', userController.updateUserInfo); // 내 정보 수정(비밀번호 변경)
 
-// 로그인
-router.post('/login', isNotLoggedIn, loginController.login);
-
-// 로그아웃
-router.post('/logout', isLoggedInForLogout, loginController.logout);
-
-// 내 정보 조회
-router.get('/', isLoggedIn, userController.findUser);
-
-// 내 정보 수정(비밀번호 변경)
-router.patch('/', isLoggedIn, userController.updateUserInfo);
+// .post('/signup', isNotLoggedIn, signupController.signup) // 회원가입
+// .post('/login', isNotLoggedIn, loginController.login) // 로그인
+// .post('/logout', isLoggedInForLogout, loginController.logout) // 로그아웃
+// .get('/', isLoggedIn, userController.findUser) // 내 정보 조회
+// .patch('/', isLoggedIn, userController.updateUserInfo); // 내 정보 수정(비밀번호 변경)
 
 module.exports = router;
